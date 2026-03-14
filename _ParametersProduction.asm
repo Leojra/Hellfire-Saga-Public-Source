a64e563c3fa3a50cc844fd084ff555f163bd2f70
Leojra <leojraiford08@gmail.com>
2026-03-13 17:35:44 -0700

    Update _ParametersProduction.asm
    Do i have to do this

--- _ParametersProduction.asm
+++ _ParametersProduction.asm
@@ -6,13 +6,13 @@
 ; This file MUST be included in git to ensure a consistent set of parameters for production.
 
 Debug_Title         = 1     ; set to 1 to enable title screen
-GameDebug           = 0     ; If 1, enable debug mode for sonic
-Debug_Lagometer     = 0     ; set to 1 to enable CPU lagometer
-Debug_EasyBoss      = 0     ; set to 1 to make every boss 1-hit kill
+GameDebug           = 1     ; If 1, enable debug mode for sonic
+Debug_Lagometer     = 1.    ; set to 1 to enable CPU lagometer
+Debug_EasyBoss      = 1.    ; set to 1 to make every boss 1-hit kill
 
-DisableSRAM         = 0     ; set to 1 to create a ROM without SRAM
+DisableSRAM         = 1    ; set to 1 to create a ROM without SRAM
 
-Debug_LevelId       = -1    ; $0202 ; set to -1 to disable debugging a level
+Debug_LevelId       = 1   ; $0202 ; set to -1 to disable debugging a level
 Debug_Xpos          = $0234 ; xpos to spawn to
 Debug_Ypos          = $0728 ; ypos to spawn to
 
